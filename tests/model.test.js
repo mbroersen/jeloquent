772ece8b9dd5600a33bfadff881f207093ff50e7
mbroersen <m.broersen@socialblue.com>
2020-10-16 23:08:56 +0200

    add other find method to test

--- tests/model.test.js
+++ tests/model.test.js
@@ -18,11 +18,13 @@ test('Simpel model setup', () => {
 
 test('User can be found', () => {
     const lUser = User.find(12);
+    const cUser = User.find([12]);
     const allUsers = User.all();
 
     expect(lUser).toBeInstanceOf(User);
     expect(lUser.primaryKey).toBe("12");
     expect(allUsers).toBeInstanceOf(Collection);
+    expect(cUser).toBeInstanceOf(Collection);
     expect(allUsers.length).toBe(1);
     expect(allUsers[0].id).toBe(12);
 })
@@ -32,4 +34,4 @@ test('Model returns classNames', () => {
     expect(User.className()).toStrictEqual("User");
     expect(User.kebabCaseClassName()).toStrictEqual("user");
     expect(User.snakeCaseClassName()).toStrictEqual("user");
-})
+});
